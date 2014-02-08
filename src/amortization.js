0a43bf2c3a6f3fbecede2401515fee610bfffefe
Stephen.England <stephenengland1986@gmail.com>
2014-02-08 15:20:55 -0600

    Added extra payment UI, interest in table, money formatting.

--- src/amortization.js
+++ src/amortization.js
@@ -1,15 +1,17 @@
 var amortization = function(ko, formulas) {
+	var todaysDate = new Date();
 	var amort = {
-		"originalPrincipal": ko.observable(209950),
-		"valueOfHome": ko.observable(220000),
-		"annualInterestRatePercent": ko.observable(3.375),
+		"originalPrincipal": ko.observable(165000),
+		"valueOfHome": ko.observable(185000),
+		"annualInterestRatePercent": ko.observable(4.5),
 		"numberOfYears": ko.observable(30),
-		"startDate": ko.observable(new Date(2013, 1, 1)),
+		"startDate": ko.observable(new Date(todaysDate.getFullYear(), todaysDate.getMonth(), 1)),
 		"schedule": ko.observableArray([]),
 		"extraPayments": new ko.observableDictionary({}),
 		"regenPayments": ko.observable(),
 		"annualTaxPercent": ko.observable(2.26),
-		"pmiPercent": ko.observable(0.5)
+		"pmiPercent": ko.observable(0.5),
+		"totalInterestPaid": ko.observable(0)
 	};
 
 	amort.annualInterestRate = ko.computed(function() {
@@ -27,51 +29,10 @@ var amortization = function(ko, formulas) {
 	amort.monthlyPayment = ko.computed(function() {
 		return formulas.getPayment(amort.originalPrincipal(), amort.monthlyInterestRate(), amort.numberOfMonths());
 	}, amort);
-
-	amort.payoffMonths = ko.computed(function() {
-		amort.schedule.removeAll();
-		var dummy = amort.regenPayments();
-		
-		var monthlyPayment = amort.monthlyPayment(),
-			currentPrincipal = amort.originalPrincipal(),
-			monthlyInterestRate = amort.monthlyInterestRate(),
-			monthNumber = 1,
-			startDate = amort.startDate();
-
-		while (currentPrincipal >= 0) {
-			var interestPayment = formulas.getCurrentInterestPayment(currentPrincipal, monthlyInterestRate);
-			var principalPayment = formulas.roundToTwo(monthlyPayment - interestPayment);
-			currentPrincipal = formulas.roundToTwo(currentPrincipal - principalPayment);
-			var newDate = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
-			newDate.setMonth(newDate.getMonth() + (monthNumber - 1));
-			
-			amort.schedule.push({
-				'principalPayment': principalPayment, 
-				'interestPayment': interestPayment,
-				'principalRemaining': currentPrincipal,
-				'monthNumber': monthNumber,
-				'date': newDate,
-				'isExtraPayment': false
-			});
-			
-			if (amort.extraPayments) {
-				var extraPayment = amort.extraPayments.get(monthNumber)();
-				if (extraPayment) {
-					currentPrincipal = (currentPrincipal - extraPayment).toFixed(2);
-					amort.schedule.push({
-						'principalPayment': extraPayment, 
-						'interestPayment': 0.00,
-						'principalRemaining': currentPrincipal,
-						'monthNumber': monthNumber,
-						'date': newDate,
-						'isExtraPayment': true
-					});
-				}
-			}
-
-			monthNumber++;
-		}
-	}, amort);
+	
+	amort.hasTwentyPercentPrincipal = function (currentPrincipal) {
+		return (amort.originalPrincipal() * 0.80) > currentPrincipal;
+	};
 	
 	amort.annualTaxRate = ko.computed(function() {
 		return amort.annualTaxPercent() / 100.00;
@@ -94,6 +55,95 @@ var amortization = function(ko, formulas) {
 	
 	amort.totalMonthlyPayment = ko.computed(function() {
 		return formulas.roundToTwo(parseFloat(amort.additionalCost()) + parseFloat(amort.monthlyPayment()));
+	}, amort);
+
+	amort.payoffDate = ko.computed(function() {
+		amort.schedule.removeAll();
+		var dummy = amort.regenPayments();
+		
+		var monthlyPayment = amort.monthlyPayment(),
+			currentPrincipal = amort.originalPrincipal(),
+			monthlyInterestRate = amort.monthlyInterestRate(),
+			monthNumber = 1,
+			startDate = amort.startDate(),
+			hasTwentyPercent = false,
+			payOffDate,
+			totalInterest = 0.0;
+
+		while (currentPrincipal >= 0) {
+			var interestPayment = formulas.getCurrentInterestPayment(currentPrincipal, monthlyInterestRate);
+			totalInterest += parseFloat(interestPayment);
+			var principalPayment = formulas.roundToTwo(monthlyPayment - interestPayment);
+			currentPrincipal = formulas.roundToTwo(currentPrincipal - principalPayment);
+			var newDate = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
+			newDate.setMonth(newDate.getMonth() + (monthNumber - 1));
+			
+			var twentyPercentEnded = false;
+			if (!hasTwentyPercent) {
+				hasTwentyPercent = amort.hasTwentyPercentPrincipal(currentPrincipal);
+				if (hasTwentyPercent) {
+					twentyPercentEnded = true;
+				}
+			}
+			
+			var escrow = hasTwentyPercent ? amort.taxPayment() : amort.additionalCost(),
+				principalComplete = currentPrincipal <= 0,
+				totalPayment;
+			
+			if (principalComplete) {
+				principalPayment = formulas.roundToTwo(parseFloat(principalPayment) + parseFloat(currentPrincipal));
+				totalPayment = formulas.roundToTwo(parseFloat(principalPayment) + parseFloat(interestPayment) + parseFloat(escrow));
+			}
+			else {
+				totalPayment = amort.totalMonthlyPayment();
+			}
+			
+			amort.schedule.push({
+				'principalPayment': principalPayment, 
+				'interestPayment': interestPayment,
+				'principalRemaining': principalComplete ? 0 : currentPrincipal,
+				'monthNumber': monthNumber,
+				'date': newDate,
+				'escrow': escrow,
+				'totalPayment': totalPayment,
+				'twentyPercentEnded': twentyPercentEnded,
+				'totalInterestPaid': formulas.roundToTwo(totalInterest),
+				'isExtraPayment': false
+			});
+			
+			if (amort.extraPayments) {
+				var extraPayment = amort.extraPayments.get(monthNumber)();
+				if (extraPayment) {
+					currentPrincipal = (currentPrincipal - extraPayment).toFixed(2);
+					twentyPercentEnded = false;
+					
+					if (!hasTwentyPercent) {
+						hasTwentyPercent = amort.hasTwentyPercentPrincipal(currentPrincipal);
+						if (hasTwentyPercent) {
+							twentyPercentEnded = true;
+						}
+					}
+					amort.schedule.push({
+						'principalPayment': extraPayment, 
+						'interestPayment': 0.00,
+						'principalRemaining': currentPrincipal,
+						'monthNumber': monthNumber,
+						'escrow': escrow,
+						'totalPayment': extraPayment,
+						'twentyPercentEnded': twentyPercentEnded,
+						'totalInterestPaid': totalInterest,
+						'date': newDate,
+						'isExtraPayment': true
+					});
+				}
+			}
+			
+			amort.totalInterestPaid(formulas.roundToTwo(totalInterest));
+			payOffDate = newDate;
+			monthNumber++;
+		}
+		
+		return payOffDate;
 	}, amort);
 	
 	return amort;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
 var amortizationFunctions = formulas(Math);
 var viewModel = amortization(ko, amortizationFunctions);
 
+var viewExtraPaymentDate = function(monthNumber) {
+	var startDate = viewModel.startDate();
+	var newDate = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
+	
+	newDate.setMonth(startDate.getMonth() + (monthNumber() - 1));
+	
+	return newDate.toLocaleDateString();
+};
+
+var getExtraPaymentMonthNumber = function(date) {
+	var startDate = viewModel.startDate();
+	
+	var monthDiff = function(d1, d2) {
+		var months = (d2.getFullYear() - d1.getFullYear()) * 12;
+		months -= d1.getMonth();
+		months += d2.getMonth();
+		return months <= 0 ? 0 : months;
+	}
+	
+	return monthDiff(startDate, date) + 1;
+};
+
 $(document).ready(function() {
 	ko.applyBindings(viewModel);
 	
-	var datepicker = $('.input-group.date').datepicker();
+	var startdatepicker = $('#startDate').datepicker(),
+		extraPaymentDate = $('#extraPaymentDate').datepicker(),
+		extraPaymentAmount = $('#extraPaymentAmount');
 	
-	datepicker.datepicker('update', viewModel.startDate());
+	startdatepicker.datepicker('update', viewModel.startDate());
 	
-	datepicker.on('changeDate', function (e) {
+	startdatepicker.on('changeDate', function (e) {
 		viewModel.startDate(e.date);
+	});
+	
+	$('#extraPaymentButton').click(function() {
+		try {
+			var monthNumber = getExtraPaymentMonthNumber(extraPaymentDate.datepicker('getDate'));
+		
+			var payment = parseInt(extraPaymentAmount.val());
+			if (payment && monthNumber) {
+				if (viewModel.extraPayments.get(monthNumber)()){
+					viewModel.extraPayments.set(monthNumber, parseInt(viewModel.extraPayments.get(monthNumber)()) + payment);
+				}
+				else {
+					viewModel.extraPayments.set(monthNumber, payment);
+				}
+			}
+		}
+		catch (err) {
+			console.log(err);
+		}
+		return false;
 	});
 });
